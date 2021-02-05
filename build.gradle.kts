358178ba5efe6fb90ec10d50cfb73f09254664f0
Project_TL <kimpiao020@gmail.com>
2021-02-05 18:21:36 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.30"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+group = properties["pluginGroup"]!!
+version = properties["pluginVersion"]!!
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+
+    jcenter()
+    maven("https://jitpack.io/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("mysql:mysql-connector-java")
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+}
+
+val shade = configurations.create("shade")
+shade.extendsFrom(configurations.implementation.get())
+
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    processResources {
+        filesMatching("*.yml") {
+            expand(project.properties)
+        }
+    }
+
+    jar {
+        from (shade.map { if (it.isDirectory) it else zipTree(it) })
+    }
+}
