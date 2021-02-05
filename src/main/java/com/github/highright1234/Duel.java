51df99ed6c985a2024f81b6871216ed768eb2cda
Sleepmin <tickiock08@gmail.com>
2021-02-05 17:35:29 +0900

    Initial commit

---
+++ src/main/java/com/github/highright1234/Duel.java
@@ -0,0 +1,17 @@
+package com.github.highright1234;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Duel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
