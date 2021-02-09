10fa509ee4d4b632b3a720f8a38c5d804f694107
Project_TL <kimpiao020@gmail.com>
2021-02-09 23:43:08 +0900

    Add: Add Test session test class.

--- src/main/java/com/github/highright1234/duel/Before.java
+++ src/main/java/com/github/highright1234/duel/Before.java
@@ -4,21 +4,24 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.projecttl.plugin.duel.DuelPlugin;
+import org.projecttl.plugin.duel.utils.CreateSessionWorld;
 import org.projecttl.plugin.duel.utils.SyncTimerWorldBoarder;
 
 public class Before {
 
-    private final DuelPlugin plugin;
-
-    private static final Location red_location = new Location(Bukkit.getWorld("duel"), 0, 0, 0);
-    private static final Location blue_location = new Location(Bukkit.getWorld("duel"), 0, 0, 0);
-    public static Player redPlayer;
-    public static Player bluePlayer;
-    public static boolean playing = false;
-
+    private DuelPlugin plugin;
     public Before(DuelPlugin plugin) {
         this.plugin = plugin;
     }
+
+    public static Player redPlayer;
+    public static Player bluePlayer;
+
+    CreateSessionWorld createSession = new CreateSessionWorld(plugin, redPlayer);
+
+    private final Location red_location = new Location(Bukkit.getWorld(createSession.getWorldName()), 0, 0, 0); // This is test
+    private final Location blue_location = new Location(Bukkit.getWorld(createSession.getWorldName()), 0, 0, 0); // This is test
+    public static boolean playing = false;
 
     public void before_setting(Player red, Player blue) {
         SyncTimerWorldBoarder syncTimer = new SyncTimerWorldBoarder(plugin);
---
+++ src/main/java/com/github/highright1234/duel/CreateSession.java
@@ -0,0 +1,22 @@
+package com.github.highright1234.duel;
+
+import org.projecttl.plugin.duel.DuelPlugin;
+
+public class CreateSession {
+
+    private final DuelPlugin plugin;
+    public CreateSession(DuelPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    /* TODO | Work start Highright. */
+
+    /* Requirement
+     * You're not add single session, must add multiple session.
+     * You're must not ignore TODO list.
+     */
+
+    public void createSession() {
+        // TODO Add logic session
+    }
+}
